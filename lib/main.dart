0b6c27550debfa1f0a3e6b5ee14c444767c50b10
unknown <josefranciscogv10@gmail.com>
2024-04-01 16:24:01 -0600

    New Folders and Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:second_service/home/home_page.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Second Service',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Second Service'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+    return MaterialApp(title: 'Second Service', home: HomePage());
   }
 }
